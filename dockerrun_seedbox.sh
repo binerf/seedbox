e845061a494be43b9df253fff11ddca5a9029e05
Binerf <binerf@mygaia.org>
2018-11-07 09:48:40 +0100

    Update

--- dockerrun_seedbox.sh
+++ dockerrun_seedbox.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-docker run -d -v /mnt/docker/seedbox/transmission-daemon:/var/lib/transmission-daemon -p 127.0.0.1:9091:9091 -p 51413:51413 binerf/seedbox
+docker run -d --name seedbox -v /mnt/docker/seedbox/transmission-daemon:/var/lib/transmission-daemon --restart always -p 127.0.0.1:9091:9091 -p 51413:51413 binerf/seedbox
